503e7c8a7bbaa960dd2483a762877f6db478284c
Bernar11296 <114645069+Bernar11296@users.noreply.github.com>
2023-04-01 12:42:48 +0600

    dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 
 	restapi "github.com/Bernar11296/rest-api"
 	"github.com/Bernar11296/rest-api/pkg/handler"
+	"github.com/Bernar11296/rest-api/pkg/repository"
+	"github.com/Bernar11296/rest-api/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repository := repository.NewRepository()
+	services := service.NewService(repository)
+	handlers := handler.NewHandler(services)
 	srv := new(restapi.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
