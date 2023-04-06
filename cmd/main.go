39d790e170db19462d48f732fcad7afdd0c5306d
Bernar <bernar_95@mail.ru>
2023-04-06 20:58:34 +0600

    viper

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,24 @@ import (
 	"github.com/Bernar11296/rest-api/pkg/handler"
 	"github.com/Bernar11296/rest-api/pkg/repository"
 	"github.com/Bernar11296/rest-api/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err:=initConfig(); err!=nil{
+		log.Fatal("error initializing configs: %s", err.Error())
+	}
 	repository := repository.NewRepository()
 	services := service.NewService(repository)
 	handlers := handler.NewHandler(services)
 	srv := new(restapi.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error{
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
