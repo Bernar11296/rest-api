9d7627f8306f0bc923acb24eaa39dd0e3d61e33f
Bernar <bernar_95@mail.ru>
2023-04-07 12:10:15 +0600

    connect to db

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"log"
+	"os"
 
 	restapi "github.com/Bernar11296/rest-api"
 	"github.com/Bernar11296/rest-api/pkg/handler"
 	"github.com/Bernar11296/rest-api/pkg/repository"
 	"github.com/Bernar11296/rest-api/pkg/service"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	if err:=initConfig(); err!=nil{
+	if err := initConfig(); err != nil {
 		log.Fatal("error initializing configs: %s", err.Error())
 	}
-	repository := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error initializing env: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repository := repository.NewRepository(db)
 	services := service.NewService(repository)
 	handlers := handler.NewHandler(services)
 	srv := new(restapi.Server)
@@ -23,7 +39,7 @@ func main() {
 	}
 }
 
-func initConfig() error{
+func initConfig() error {
 	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
